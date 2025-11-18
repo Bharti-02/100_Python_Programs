6364188462c848c37394575ae8ce57ab0fe82c16
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-11-18 22:25:04 +0530

    Add files via upload

---
+++ 100-Python_Programs/Area_of_Triangle.py
@@ -0,0 +1,8 @@
+# Calculate the Area of triangle
+
+def Triangle(l,b):
+	return 0.5 * l * b
+
+l=int(input("Enter Length:"))
+b=int(input("Enter Breadth:"))
+print("Area of Triangle :",Triangle(l,b))
